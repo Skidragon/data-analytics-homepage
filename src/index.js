09fc50b303d13a98209ca6fed0e402e6de378ef0
Simon Davis <Simon_Freelance@hotmail.com>
2021-05-16 12:57:23 -0400

    Able to get menu to open and close with overlapping fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { MenuFactory } from "./menu";
 
 const menu = MenuFactory(document.querySelector(".menu"));
-// const openMenuBtn = document.querySelector(".open-menu-btn");
+const openMenuBtn = document.querySelector(".open-menu-btn");
 const closeMenuBtn = document.querySelector(".close-menu-btn");
-// menu.addOpenTrigger(openMenuBtn);
+menu.addOpenTrigger(openMenuBtn);
 menu.addCloseTrigger(closeMenuBtn);
--- src/menu.js
+++ src/menu.js
@@ -1,14 +1,24 @@
 export const MenuFactory = (menu) => {
+  const openMenu = () => {
+    menu.style.display = "block";
+    menu.style.position = "fixed";
+    menu.style.zIndex = 1000;
+  };
+  const closeMenu = () => {
+    menu.style.display = "none";
+  };
   return {
     addOpenTrigger: (trigger) => {
-      trigger.addEventListener("click", function () {
-        menu.style.display = "static";
-      });
+      trigger.addEventListener("click", openMenu);
     },
     addCloseTrigger: (trigger) => {
-      trigger.addEventListener("click", function () {
-        menu.style.display = "none";
-      });
+      trigger.addEventListener("click", closeMenu);
+    },
+    removeOpenTrigger: (trigger) => {
+      trigger.removeEventListener("click", openMenu);
+    },
+    removeCloseTrigger: (trigger) => {
+      trigger.removeEventListener("click", closeMenu);
     }
   };
 };
